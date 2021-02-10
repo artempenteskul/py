7588dc6992c530b9777307e08b1b30be5b46aa66
tommy <artemiy.penteskul@gmail.com>
2021-02-11 00:04:17 +0200

    'test'

--- main.py
+++ main.py
@@ -1,78 +1,112 @@
-print('Hello World')
-print("Hello Python")
-print(4)
-arr = [66, 'hello', 9, 'volodya', 11, 856.4]
-print(arr)
-dictionary = {'value': 5}
-tup = (66, 'ddd', 44)
-
-# Integer
-
-print(7 + 4)
-print(100 - 85)
-print(2 * 2)
-print(2 / 2)
-print(2 ** 7)
-print(8 / 6)
-print(8 // 6)
-print(8 % 6)
-
-# Переменные
-
-x = 'hhhh'
-print(x)
-x = 5
-print(x)
-x = []
-print(type(x))
-
-car_color = 'black'
-carColor = 'black'
-print(carColor)
-
-a = 6
-b = 5
-print(a * b)
-
-
-# Str
-
-greet = 'Hello'
-firstName = 'Alex'
-lastName = 'White'
-print(greet + ' ' + firstName + ' ' + lastName)
-
-longStr = 'This is long str'
-print(longStr)
-
-#Escaping
-
-someStr = 'I\'m programmer'
-name = '"Fanta"'
-print(name)
-print('h\ne\nl\nl\no')
-
-nums = '12\t34\t567'
-print(nums)
-
-#Triple quotes
-
-triple = """This is text with triple quotes"""
-print(triple)
-
-greeting = 'Hello Python!'
-print(len(greeting))
-print(greeting[3])
-print(greeting[-3])
-
-# Slice
-
-print(greeting[2:8])
-print(greeting[-7:-1])
-print(greeting[:])
-print(greeting[1:9:2])
+# print('Hello World')
+# print("Hello Python")
+# print(4)
+# arr = [66, 'hello', 9, 'volodya', 11, 856.4]
+# print(arr)
+# dictionary = {'value': 5}
+# tup = (66, 'ddd', 44)
+#
+# # Integer
+#
+# print(7 + 4)
+# print(100 - 85)
+# print(2 * 2)
+# print(2 / 2)
+# print(2 ** 7)
+# print(8 / 6)
+# print(8 // 6)
+# print(8 % 6)
+#
+# # Переменные
+#
+# x = 'hhhh'
+# print(x)
+# x = 5
+# print(x)
+# x = []
+# print(type(x))
+#
+# car_color = 'black'
+# carColor = 'black'
+# print(carColor)
+#
+# a = 6
+# b = 5
+# print(a * b)
+#
+#
+# # Str
+#
+# greet = 'Hello'
+# firstName = 'Alex'
+# lastName = 'White'
+# print(greet + ' ' + firstName + ' ' + lastName)
+#
+# longStr = 'This is long str'
+# print(longStr)
+#
+# #Escaping
+#
+# someStr = 'I\'m programmer'
+# name = '"Fanta"'
+# print(name)
+# print('h\ne\nl\nl\no')
+#
+# nums = '12\t34\t567'
+# print(nums)
+#
+# #Triple quotes
+#
+# triple = """This is text with triple quotes"""
+# print(triple)
+#
+# greeting = 'Hello Python!'
+# print(len(greeting))
+# print(greeting[3])
+# print(greeting[-3])
+#
+# # Slice
+#
+# print(greeting[2:8])
+# print(greeting[-7:-1])
+# print(greeting[:])
+# print(greeting[1:9:2])
 
 # Methods Str
 
 
+# boxes = {}
+# jars = {}
+# crates = {}
+#
+# boxes['c'] = 1
+# boxes['x'] = 2
+# jars['h'] = 4
+#
+# crates['boxes'] = boxes
+# crates['jars'] = jars
+#
+# print(len(crates[boxes]))
 
+# print(r'\nwoow')
+#
+# print(0.1 + 0.2 == 0.3)
+#
+# print(0xA + 0xa)
+#
+# mt = (1, 2, 3, 4)
+# mt.append((5, 6, 7))
+# print(len(mt))
+#
+
+# kvps = {'1': 1, '2':2}
+# theCopy = kvps
+#
+# kvps['1'] = 5
+#
+# sum = kvps['1'] + theCopy['1']
+# print(sum)
+
+names = ['a', 'b']
+loc = names.index('e')
+print(loc)
