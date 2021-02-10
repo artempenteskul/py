106fe6e13cfa1b41f337536e94b84f546b5b2c1e
tommy <artemiy.penteskul@gmail.com>
2021-02-10 20:19:38 +0200

    'pypy'

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
 print('Hello World')
 print("Hello Python")
 print(4)
+arr = [66, 'hello', 9, 'volodya', 11, 856.4]
+print(arr)
+dictionary = {'value': 5}
+tup = (66, 'ddd', 44)
 
+# Integer
+
+print(7 + 4)
+print(100 - 85)
+print(2 * 2)
+print(2 / 2)
+print(2 ** 7)
+print(8 / 6)
+print(8 // 6)
+print(8 % 6)
+
+# Переменные
+
+x = 'hhhh'
+print(x)
+x = 5
+print(x)
+x = []
+print(type(x))
+
+car_color = 'black'
+carColor = 'black'
+print(carColor)
+
+a = 6
+b = 5
+print(a * b)
