ca3dab9587a43219eb8a900799081c59564badee
tommy <artemiy.penteskul@gmail.com>
2021-02-12 23:03:01 +0200

    'sss'

--- py.py
+++ py.py
@@ -18,3 +18,30 @@ print(seconds_per_day)
 print(seconds_per_day / seconds_per_hour)
 print(seconds_per_day // seconds_per_hour)
 
+# Second
+
+years_list = [2002, 2003, 2004, 2005, 2006, 2007]
+print(years_list[3])
+five = years_list[5]
+
+things = ['mozarella', 'cinderella', 'salmonella']
+things[2] = 'Cinderella'
+print(things)
+things[1] = things[1].upper()
+print(things)
+del things[2]
+print(things)
+
+surprise = ['Groucho', 'Chico', 'Harpo']
+surprise[2] = surprise[2].lower()
+
+e2f = {
+    'dog': 'chien',
+    'cat': 'chat',
+    'walrus': 'morse'
+}
+
+f2e = list(e2f.items())
+print(f2e)
+
+print(e2f.keys())
