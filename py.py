72de9a7a2fbc145e64b5ca1d0cd3662cd730dcbf
tommy <artemiy.penteskul@gmail.com>
2021-02-12 20:48:55 +0200

    'fff'

--- py.py
+++ py.py
@@ -1,2 +1,20 @@
-# Карманный справочник Python
+# Simple Python
+
+# print(divmod(9, 4))
+#
+# print(int(98.4))
+
+# start = 'na' * 4 + '\n'
+# middle = 'hey' * 3 + '\n'
+# end = 'goodbye'
+
+#print(start + start + middle + end)
+
+# First
+seconds_per_hour = 60 * 60
+print(seconds_per_hour)
+seconds_per_day = seconds_per_hour * 24
+print(seconds_per_day)
+print(seconds_per_day / seconds_per_hour)
+print(seconds_per_day // seconds_per_hour)
 
