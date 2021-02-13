5685b417260038ded98efa2cdae46c39a0453583
tommy <artemiy.penteskul@gmail.com>
2021-02-13 19:41:56 +0200

    '4'

--- py.py
+++ py.py
@@ -8,7 +8,7 @@
 # middle = 'hey' * 3 + '\n'
 # end = 'goodbye'
 
-#print(start + start + middle + end)
+# print(start + start + middle + end)
 
 # First
 seconds_per_hour = 60 * 60
@@ -46,7 +46,57 @@ print(f2e)
 
 print(e2f.keys())
 
-
 # Third
+guess_me = 7
+
+if guess_me > 7:
+    print('too more')
+elif guess_me < 7:
+    print('too low')
+else:
+    print('its 7')
+
+start = 1
+
+while guess_me > start:
+    print('too low')
+    start += 1
+    if guess_me == start:
+        print('found it')
+        break
+
+arrs = [3, 2, 1, 0]
+
+for arr in arrs:
+    print(arr)
+
+a = [num for num in range(10) if num % 2 == 0]
+print(a)
+
+a = list(range(10))
+
+# gg
+squares = {key: key * key for key in range(10)}
+print(squares)
+
+odd = {num for num in range(10) if num % 2 == 1}
+print(odd)
+
+# gg
+for thing in ('got %s' % num for num in range(10)):
+    print(thing)
 
 
+def good():
+    return ['Harry', 'Ron', 'Hermione']
+
+
+def get_odds():
+    for num in (1, 10, 2):
+        yield num
+
+
+for count, num in enumerate(get_odds(), 1):
+    if count == 3:
+        print('The third odd num is', num)
+        break
