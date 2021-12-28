8eb5828d491638fef41b40afb72b5feb4f97b251
Artem Penteskul <artempenteskul@Macs-MacBook-Pro.local>
2021-12-29 00:32:31 +0200

    ten exercises

---
+++ basic/arithmetic.py
@@ -0,0 +1,13 @@
+from math import log10
+
+
+a = int(input('Enter A number: '))
+b = int(input('Enter B number: '))
+
+print(f'A + B: {a + b}')
+print(f'A - B: {a - b}')
+print(f'A * B: {a * b}')
+print(f'A / B: {a / b}')
+print(f'A % B: {a % b}')
+print(f'Log10A: {log10(a)}')
+print(f'A ** B: {a ** b}')
---
+++ basic/room.py
@@ -0,0 +1,4 @@
+length = int(input('Enter length of your room: '))
+width = int(input('Enter width of your room: '))
+
+print('Room square is', length * width, 'metres')
