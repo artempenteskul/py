921a2997ea6e68653210366d11733150b757a731
Artem Penteskul <artempenteskul@Macs-MacBook-Pro.local>
2022-03-13 23:58:39 +0200

    refactoring

--- codewars/simple_pig_latin.py
+++ codewars/simple_pig_latin.py
@@ -4,16 +4,15 @@ punctuation_marks = ('!', ':', ',', '?')
 def pig_it(text):
     words = text.split()
     res_words = []
-    print(words)
     for word in words:
         if word in punctuation_marks:
             res_words.append(word)
         else:
             res_word = word[1:] + word[0] + 'ay'
-            print(res_word)
             res_words.append(res_word)
     return ' '.join(res_words)
 
 
 if __name__ == '__main__':
-    print(pig_it('Pig latin is cool'))
+    users_text = (input('Enter your text: '))
+    print(pig_it(users_text))
