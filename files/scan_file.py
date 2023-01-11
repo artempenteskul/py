e7d76f6a4f3b605898f1ff3c8f88159a5815af73
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-01-11 23:10:33 +0200

    modified scan_file script

--- files/scan_file.py
+++ files/scan_file.py
@@ -1,9 +1,15 @@
+import sys
+
+
 def scanner(filename, function):
     file = open(filename, 'r')
     while True:
         line = file.readline()
         if not line:
             break
-        function(line)
+        print(function(line))
     file.close()
 
+
+if __name__ == '__main__':
+    scanner(sys.argv[1], str.upper)
