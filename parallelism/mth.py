6578dbb650fb1d25ecb9f99bf9f4429e08a44a93
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-07-15 17:44:02 +0300

    refactored mth module

--- parallelism/mth.py
+++ parallelism/mth.py
@@ -1,22 +1,42 @@
 # multithreading example in python
 
 import time
+import random
 import threading as th
 
 
-def func(data: str) -> None:
-    print(f'data - {data}')
+def func() -> None:
+    print(f'RESULT - {random.randint(0, 10)}')
     time.sleep(2)  # here should be something like http call
 
 
 if __name__ == '__main__':
+    """
+    this work in sync mode without threads
+    """
+
     start = time.time()
 
-    th1 = th.Thread(target=func, args=('text', ))
-    th2 = th.Thread(target=func, args=('text', ))
-    th3 = th.Thread(target=func, args=('text', ))
+    for i in range(3):
+        func()
 
-    # start three threads (on time.sleep() gil is not blocked and return the control to main thread)
+    end = time.time()
+
+    print(end - start)
+
+
+if __name__ == '__main__':
+    """
+    this works by spamming three threads
+    """
+
+    start = time.time()
+
+    th1 = th.Thread(target=func)
+    th2 = th.Thread(target=func)
+    th3 = th.Thread(target=func)
+
+    # start three threads (on time.sleep() gil is not blocked and return the control to other threads)
     for t in (th1, th2, th3):
         t.start()
 
